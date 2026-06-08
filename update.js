6b7fda4a76104d967a28fa989a05f0518346e48f
6Morpheus6 <wanton-decay@gmx.net>
2026-06-08 21:14:43 +0200

    update
    - call torch.js (ensures right torch version)
    - install node modules, update and  build

--- update.js
+++ update.js
@@ -21,6 +21,28 @@ module.exports = {
         // "uv pip install hf_xet"  // stalls model downloads for me.  delete the 1st '//' to install on update
       ]
     }
-  },   
- ]
+  }, {
+    method: "script.start",
+    params: {
+      uri: "torch.js",
+      params: {
+        venv: "env",
+        path: "app",
+      }
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      path: "app/ui",
+      message: [
+        "npm install",
+        "npm run update_db",
+        "npm run build"
+      ],
+      on: [{
+        event: "/error:/i",
+        break: false
+      }]
+    }
+  }]
 }
